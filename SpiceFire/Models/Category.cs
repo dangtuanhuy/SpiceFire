40caf58326da891dff204d3e3cdcc78eb9074d4b
Unknown <huyb1505883@student.ctu.edu.vn>
2019-01-31 19:36:11 +0700

    Update Source

---
+++ SpiceFire/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpiceFire.Models
+{
+    public class Category
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Category can not null")]
+        [Display(Name = "Category")]
+        public string Name { get; set; }
+    }
+}
