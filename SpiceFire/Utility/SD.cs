bcee5547f4602223edd991b611739d8418844168
Unknown <huyb1505883@student.ctu.edu.vn>
2019-02-02 10:46:51 +0700

    MenuItem Complete

---
+++ SpiceFire/Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpiceFire.Utility
+{
+    public static class SD
+    {
+        public const string DefaultFoodImg = "default_food.png";
+    }
+}
